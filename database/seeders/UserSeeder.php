7a2e333e73feae99c70ff3d16358374c264bdb55
rismafadila <rismaf651@gmail.com>
2021-12-23 14:07:06 +0700

    Role admin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use App\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -15,40 +16,66 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+
+
+        // $user = new User();
+        // $user->name = 'Risma Fadila';
+        // $user->email = 'admin@gmail.com';
+        // $user->password = Hash::make('12345678');
+        // $user->save();
+
+        // $user = new User();
+        // $user->name = 'admin';
+        // $user->email = 'admin1@gmail.com';
+        // $user->password = Hash::make('22345678');
+        // $user->save();
+
+        // $user = new User();
+        // $user->name = 'admin';
+        // $user->email = 'admin2@gmail.com';
+        // $user->password = Hash::make('32345678');
+        // $user->save();
+
+        // $user = new User();
+        // $user->name = 'admin';
+        // $user->email = 'admin3@gmail.com';
+        // $user->password = Hash::make('42345678');
+        // $user->save();
+
+        // $user = new User();
+        // $user->name = 'admin';
+        // $user->email = 'admin4@gmail.com';
+        // $user->password = Hash::make('52345678');
+        // $user->save();
+
+        // $user = new User();
+        // $user->name = 'admin';
+        // $user->email = 'admin5@gmail.com';
+        // $user->password = Hash::make('62345678');
+        // $user->save();
+        $admin = Role::create([
+            'name' => 'admin',
+            'display_name' => 'User Administrator'
+            ]);
+
+            $pengguna = Role::create([
+            'name' => 'pengguna',
+            'display_name' => 'User Biasa'
+            ]);
+
         $user = new User();
         $user->name = 'Risma Fadila';
         $user->email = 'admin@gmail.com';
         $user->password = Hash::make('12345678');
         $user->save();
 
-        $user = new User();
-        $user->name = 'admin';
-        $user->email = 'admin1@gmail.com';
-        $user->password = Hash::make('22345678');
-        $user->save();
+        $pengunjung = new User();
+        $pengunjung->name = 'pengunjung';
+        $pengunjung->email = 'pengunjung@gmail.com';
+        $pengunjung->password = Hash::make('12345678');
+        $pengunjung->save();
 
-        $user = new User();
-        $user->name = 'admin';
-        $user->email = 'admin2@gmail.com';
-        $user->password = Hash::make('32345678');
-        $user->save();
-
-        $user = new User();
-        $user->name = 'admin';
-        $user->email = 'admin3@gmail.com';
-        $user->password = Hash::make('42345678');
-        $user->save();
-
-        $user = new User();
-        $user->name = 'admin';
-        $user->email = 'admin4@gmail.com';
-        $user->password = Hash::make('52345678');
-        $user->save();
-
-        $user = new User();
-        $user->name = 'admin';
-        $user->email = 'admin5@gmail.com';
-        $user->password = Hash::make('62345678');
-        $user->save();
+        $user->attachRole($admin);
+        $pengunjung->attachRole($pengguna);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,25 @@ Auth::routes(
 );
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//hanya untuk role admin
+Route::group(['prefix' => 'admin','middleware' => ['auth','role:admin']], function(){
+    Route::get('/', function(){
+        return 'halaman admin';
+    });
+
+    Route::get('profile', function(){
+        return 'halaman profile admin';
+    });
+});
+
+//hanya untuk role pengguna
+Route::group(['prefix' => 'pengguna','middleware' => ['auth','role:pengguna']], function(){
+    Route::get('/', function(){
+        return 'halaman pengguna';
+    });
+
+    Route::get('profile', function(){
+        return 'halaman profile pengguna';
+    });
+});
