f6d26d2e46ade63d4b2e37fe2dfb2eeff1d9dfb9
rismafadila <rismaf651@gmail.com>
2021-12-23 07:44:18 +0700

    5 data user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,5 +20,35 @@ class UserSeeder extends Seeder
         $user->email = 'admin@gmail.com';
         $user->password = Hash::make('12345678');
         $user->save();
+
+        $user = new User();
+        $user->name = 'admin';
+        $user->email = 'admin1@gmail.com';
+        $user->password = Hash::make('22345678');
+        $user->save();
+
+        $user = new User();
+        $user->name = 'admin';
+        $user->email = 'admin2@gmail.com';
+        $user->password = Hash::make('32345678');
+        $user->save();
+
+        $user = new User();
+        $user->name = 'admin';
+        $user->email = 'admin3@gmail.com';
+        $user->password = Hash::make('42345678');
+        $user->save();
+
+        $user = new User();
+        $user->name = 'admin';
+        $user->email = 'admin4@gmail.com';
+        $user->password = Hash::make('52345678');
+        $user->save();
+
+        $user = new User();
+        $user->name = 'admin';
+        $user->email = 'admin5@gmail.com';
+        $user->password = Hash::make('62345678');
+        $user->save();
     }
 }
