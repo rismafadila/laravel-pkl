988a1bedf284ba9b1c418d8189c64e511b958239
rismafadila <rismaf651@gmail.com>
2022-01-12 10:40:39 +0700

    menambah tampilan home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -94,56 +94,123 @@
   </div>
 </div>
 
-<table>
-  <center><i><h4>Atasan</h4></i></center>
+{{-- <table>
+  <center><i><h4><b>FASILITAS</b></h4></i></center>
    <tr>
    <td>
   <div class="card" >
-      <img src="{{ asset('assets/img/3.jpg') }}" class="card-img-top" style="width:70%;  height:70%;">
-      
+      <img src="{{ asset('assets/img/1.jfif') }}" class="card-img-top" style="width:50%;  height:50%;">
+
     </div>
   </td>
   <td>
   <div class="card" >
-      <img src="{{ asset('assets/img/3.jpg') }}" class="card-img-top" style="width:70%;  height:70%;">
-      
+      <img src="{{ asset('assets/img/3.jfif') }}" class="card-img-top" style="width:50%;  height:50%;">
+
     </div>
   </td>
   <td>
       <div class="card" >
-          <img src="{{ asset('assets/img/3.jpg') }}" class="card-img-top" style="width:70%;  height:70%;">
-          
+          <img src="{{ asset('assets/img/4.jfif') }}" class="card-img-top" style="width:50%;  height:50%;">
+
         </div>
       </td>
-      
+
   </tr>
 
-</table>
-<table>
-  
+
+
    <tr>
    <td>
   <div class="card" >
-      <img src="{{ asset('assets/img/3.jpg') }}" class="card-img-top" style="width:70%;  height:70%;">
-      
+      <img src="{{ asset('assets/img/5.jfif') }}" class="card-img-top" style="width:50%;  height:50%;">
+
     </div>
   </td>
   <td>
   <div class="card" >
-      <img src="{{ asset('assets/img/3.jpg') }}" class="card-img-top" style="width:70%;  height:70%;">
-      
+      <img src="{{ asset('assets/img/6.jfif') }}" class="card-img-top" style="width:50%;  height:50%;">
+
     </div>
   </td>
   <td>
       <div class="card" >
-          <img src="{{ asset('assets/img/3.jpg') }}" class="card-img-top" style="width:70%;  height:70%;">
-          
+          <img src="{{ asset('assets/img/8.jpg') }}" class="card-img-top" style="width:50%;  height:50%;">
+
         </div>
       </td>
-      
+
   </tr>
 
-</table>
+</table> --}}
+
+<div class="card-columns">
+  <div class="card">
+    <img class="card-img-top" src="{{ asset('assets/img/1.jfif') }}" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title"><b>Kerja Sama Dunia Industri</b> </h5>
+      <p class="card-text">Lebih dari 100 MoU Dengan Dunia Industri dan Dunia Usaha Sehingga
+          Kurikulum Yang Diterapkan Merupakan Hasil Musyawarah DU/DI.</p>
+    </div>
+  </div>
+  <div class="card p-3">
+    <blockquote class="blockquote mb-0 card-body">
+      <p><b>Comfortable Study Facillities</b></p>
+      <footer class="blockquote-footer">
+        <small class="text-muted">
+          Someone famous in <cite title="Source Title">Source Title</cite>
+        </small>
+      </footer>
+    </blockquote>
+  </div>
+  <div class="card">
+    <img class="card-img-top" src="{{ asset('assets/img/3.jfif') }}" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title"><b>Fasilitas</b></h5>
+      <p class="card-text">Memiliki Unit Produksi SMK Assalaam Yang Menjadi Laboratorium Langsung Bagi Siswa
+          Dalam Mengembangkan Keterampilan Kejuruannya. </p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card bg-primary text-white text-center p-3">
+    <blockquote class="blockquote mb-0">
+      <p>Comfortable Study Facillities</p>
+      <footer class="blockquote-footer">
+        <small>
+          Someone famous in <cite title="Source Title">Source Title</cite>
+        </small>
+      </footer>
+    </blockquote>
+  </div>
+  <div class="card text-center">
+    <div class="card-body">
+      <h5 class="card-title"><b>Lulusan</b></h5>
+      <p class="card-text">Lulusan SMK Assalaam Dibekali Dengan Kemampuan Yang Disesuaikan Kebutuhan Industri. </p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img class="card-img" src="{{ asset('assets/img/5.jfif') }}" alt="Card image">
+  </div>
+  <div class="card p-3 text-right">
+    <blockquote class="blockquote mb-0">
+      <p>Berdiri Pada Tanggal <b>2 Mei 2009</b> Bertepatan Tanggal 7 Jumadil Awal 1430 H.</p>
+      <footer class="blockquote-footer">
+        <small class="text-muted">
+          Someone famous in <cite title="Source Title">Source Title</cite>
+        </small>
+      </footer>
+    </blockquote>
+  </div>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Motto SMK Assalaam</h5>
+      <p class="card-text">kesuksesan diawali oleh perubahan, perubahan terjadi pada orang yang aktif,
+          kreatif dan inovatif.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
 
   <!-- Footer -->
 <footer class="text-center text-lg-start bg-dark text-muted">
