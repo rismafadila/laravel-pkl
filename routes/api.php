84b831faa5dfbfb97b540d9bea356c12bd39a95d
rismafadila <rismaf651@gmail.com>
2022-03-24 10:08:22 +0700

    API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-use App\Http\Controllers\API\DataController;
+use App\Http\Controllers\API\KategoriController;
 use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,5 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 //route API
-Route::resource('data_barang',DataController::class);
+Route::resource('kategori',KategoriController::class);
 Route::resource('users',UserController::class);
